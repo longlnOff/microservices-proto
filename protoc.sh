48e347c6abf75ffecc652cd198e020958232ac57
longln <longlnofficial@gmail.com>
2025-04-16 22:52:26 +0700

    add remove service folder and init new

--- protoc.sh
+++ protoc.sh
@@ -13,6 +13,7 @@ sudo apt-get install -y protobuf-compiler golang-goprotobuf-dev
 go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
 
+rm -rf ./golang/${SERVICE_NAME}
 # Create output directory if it doesn't exist
 mkdir -p ./golang/${SERVICE_NAME}
 
